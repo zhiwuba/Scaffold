2441902ed80e7c90d92431a71c2d9142a0e7c79b
explorer <xiabingliu@163.com>
2016-03-09 01:23:02 +0800

    add elasticsearch protocal builder

---
+++ scaffold/Database/Query/ElastciSearch/Boolean.php
@@ -0,0 +1,36 @@
+<?php
+/*
+* This file is part of the Scaffold package.
+*
+* (c) bingxia liu  <xiabingliu@163.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+namespace Scaffold\Database\Query\ElasticSearch;
+
+
+class Boolean
+{
+    protected $causes;
+
+    public function addShould()
+    {
+
+    }
+
+    public function addMust()
+    {
+
+    }
+
+    public function addMustNot()
+    {
+
+    }
+
+    public function addFilter()
+    {
+        //TODO
+    }
+}
---
+++ scaffold/Database/Query/ElastciSearch/Must.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* This file is part of the Scaffold package.
+*
+* (c) bingxia liu  <xiabingliu@163.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+namespace Scaffold\Database\Query\ElasticSearch;
+
+class Must extends Logic
+{
+
+}
